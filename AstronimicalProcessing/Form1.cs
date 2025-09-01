ffda6baac80dc527fb0ffcf4c53068bb572a3568
Raph <30099423@tafe.wa.edu.au>
2025-09-01 09:57:34 +0800

    Initial

---
+++ AstronimicalProcessing/Form1.cs
@@ -0,0 +1,9 @@
+namespace AstronimicalProcessing;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# C# ONE AT2 (Sprint 1)  
+Program for AT2 of C# ONE cluster.
