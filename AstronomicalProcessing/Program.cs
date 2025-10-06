d337f6d2daf2559ab4fc82aca98a863f9eefe112
Raph <30099423@tafe.wa.edu.au>
2025-10-06 08:56:11 +0800

    Removed save on close

--- AstronomicalProcessing/Program.cs
+++ AstronomicalProcessing/Program.cs
@@ -11,8 +11,6 @@ static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        var mainForm = new AstronomicalProcessing();
-        Application.Run(mainForm);
-        //mainForm.SaveFile();
+        Application.Run(new AstronomicalProcessing());
     }
 }
