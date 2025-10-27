b022d068bfe3e7862f67aeca721980af8b3119ff
Raph <raphaelf149@gmail.com>
2025-10-27 06:05:40 -0700

    Fix: calls saved mid extreme calculation to display instead of calculating again

--- AstronomicalProcessing/APForm.cs
+++ AstronomicalProcessing/APForm.cs
@@ -183,7 +183,7 @@ namespace AstronomicalProcessing
         {
             float mid = calc.MidExtreme();
             string format = mid == (int)mid ? "f0" : "f2";
-            TextBoxMidExtreme.Text = calc.MidExtreme().ToString(format);
+            TextBoxMidExtreme.Text = mid.ToString(format);
         }
 
         /// <summary>
